27feacbb47bde88b38039088bd0445b3b961c78b
DESKTOP-MKH8SQO\quock <quockhanhtruong166@gmail.com>
2023-02-08 17:27:05 +0700

    fix slider bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const swiperLoop = new Swiper(
-  "#banner .swiper, #testimonials .swiper, .m-client .swiper, #client .swiper, #people .swiper",
+  "#banner .banner-m, #client .swiper.client-m, #people .swiper, #testimonials .testimonials-m",
   {
     // Optional parameters
     direction: "horizontal",
@@ -28,5 +28,44 @@ const swiper = new Swiper(".m-category.swiper, #vision .swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+const bannerSwiper = new Swiper(" #banner .banner-p", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 200,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+});
+
+const clientSwiper = new Swiper("#client .swiper.client-pc", {
+  direction: "horizontal",
+  slidesPerView: 6,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: { delay: 3000 },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+});
+
+const testimonialsSwiper = new Swiper("#testimonials .testimonials-p", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: { delay: 3000 },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
   },
 });
